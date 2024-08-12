9b75c90e7dfc27279269c61d03b71d096bf18322
gogojjh <jiaojh1994@gmail.com>
2024-08-12 16:00:11 +0100

    add VISLocalizationInterface for visual localization

--- include/navigation_interface/localization_interface.h
+++ include/navigation_interface/localization_interface.h
@@ -53,9 +53,9 @@ tf::Transform convertEigenMatrixToTransform(const Eigen::Matrix4d &matrix) {
   return transform;
 }
 
-class LocalizationInterface {
+class CMULocalizationInterface {
  public:
-  LocalizationInterface(ros::NodeHandle &nh, ros::NodeHandle &nhp)
+  CMULocalizationInterface(ros::NodeHandle &nh, ros::NodeHandle &nhp)
       : sub_odometry(nh, "/Odometry", 10),
         sub_pointcloud(nh, "/cloud_registered_body", 10),
         sync(SyncPolicy(10), sub_odometry, sub_pointcloud) {
@@ -88,7 +88,7 @@ class LocalizationInterface {
 
     tf_listener.reset(new tf::TransformListener);
 
-    sync.registerCallback(boost::bind(&LocalizationInterface::callback, this, _1, _2));
+    sync.registerCallback(boost::bind(&CMULocalizationInterface::callback, this, _1, _2));
     // clang-format on
   }
 
@@ -238,4 +238,138 @@ class LocalizationInterface {
   bool init_system = false;
   int odom_cnt = 0;
   Eigen::Matrix4d T_base_pointcloud = Eigen::Matrix4d::Identity();
+};
+
+class VISLocalizationInterface {
+ public:
+  VISLocalizationInterface(ros::NodeHandle &nh, ros::NodeHandle &nhp){
+    // clang-format off
+    sub_odometry = nh.subscribe("/Odometry", 10, &VISLocalizationInterface::OdomCallback, this);
+
+    loc_world_frame_id = get_ros_param(nhp, "loc_world_frame_id", std::string("vloc_map"));
+    loc_sensor_frame_id = get_ros_param(nhp, "loc_sensor_frame_id", std::string("sensor"));
+
+    world_frame_id = get_ros_param(nhp, "world_frame_id", std::string("map"));      // the world frame of the base
+    sensor_frame_id = get_ros_param(nhp, "sensor_frame_id", std::string("sensor")); // the frame id of the sensor
+    base_frame_id = get_ros_param(nhp, "base_frame_id", std::string("base"));
+
+    path_msg.poses.clear();
+    path_msg.poses.reserve(10000);
+
+#ifdef DEBUG
+    std::cout << "loc_world_frame_id: " << loc_world_frame_id << std::endl;
+    std::cout << "loc_sensor_frame_id: " << loc_sensor_frame_id << std::endl;
+    std::cout << "world_frame_id: " << world_frame_id << std::endl;
+    std::cout << "sensor_frame_id: " << sensor_frame_id << std::endl;
+    std::cout << "base_frame_id: " << base_frame_id << std::endl;
+#endif
+
+    pub_state_estimation = nh.advertise<nav_msgs::Odometry>("/vloc/state_estimation", 1);
+    pub_path = nh.advertise<nav_msgs::Path>("/vloc/state_estimation_path", 1);
+
+    tf_listener.reset(new tf::TransformListener);
+    // clang-format on
+  }
+
+  void OdomCallback(const nav_msgs::Odometry::ConstPtr &odom_msg) {
+#ifdef DEBUG
+    std::cout << "Odometry callback triggered" << std::endl;
+#endif
+    if (!init_system) {
+      try {
+        tf::StampedTransform transform_base_sensor;
+        tf_listener->lookupTransform(base_frame_id, sensor_frame_id,
+                                     ros::Time(0), transform_base_sensor);
+        T_base_sensor = convertTransformToEigenMatrix(transform_base_sensor);
+        init_system = true;
+#ifdef DEBUG
+        std::cout << "Initial system initialized" << std::endl;
+        std::cout << "T_base_sensor:\n" << T_base_sensor << std::endl;
+#endif
+      } catch (tf::TransformException &ex) {
+        return;
+      }
+    }
+    if (init_system) {
+      Eigen::Matrix4d T_world_base_base = processOdometry(odom_msg);
+    }
+  }
+
+ private:
+  Eigen::Matrix4d processOdometry(const nav_msgs::Odometry::ConstPtr &msg) {
+#ifdef DEBUG
+    std::cout << "Processing odometry" << std::endl;
+#endif
+    // This is the difference from the CMULocalizationInterface
+    // CMULocalizationInterface: world_imu_link of the localization odometry should be changed to the world_base frame
+    // VISLocalizationInterface: the localization odometry is already based on the world_base frame
+    // NOTE(gogojjh): T_world_base_sensor: the transformation from the world frame to the sensor frame
+    Eigen::Matrix4d T_world_base_sensor = Eigen::Matrix4d::Identity();
+    Eigen::Vector3d position(msg->pose.pose.position.x,
+                             msg->pose.pose.position.y,
+                             msg->pose.pose.position.z);
+    T_world_base_sensor.block<3, 1>(0, 3) = position;
+    Eigen::Quaterniond quat(
+        msg->pose.pose.orientation.w, msg->pose.pose.orientation.x,
+        msg->pose.pose.orientation.y, msg->pose.pose.orientation.z);
+    T_world_base_sensor.block<3, 3>(0, 0) = quat.toRotationMatrix();
+    // NOTE(gogojjh): T_world_base_base: the transformation from the world_base frame to the base frame
+    Eigen::Matrix4d T_world_base_base = T_world_base_sensor * T_base_sensor.inverse();
+    // Publish the odometry
+    // TODO(gogojjh): convert the covariance to the odometry
+    nav_msgs::Odometry transformed_odom;
+    transformed_odom.header.stamp = msg->header.stamp;
+    transformed_odom.header.frame_id = world_frame_id;
+    transformed_odom.child_frame_id = base_frame_id;
+    transformed_odom.pose.pose.position.x = T_world_base_base(0, 3);
+    transformed_odom.pose.pose.position.y = T_world_base_base(1, 3);
+    transformed_odom.pose.pose.position.z = T_world_base_base(2, 3);
+    Eigen::Quaterniond transformed_quat(T_world_base_base.block<3, 3>(0, 0));
+    transformed_odom.pose.pose.orientation.w = transformed_quat.w();
+    transformed_odom.pose.pose.orientation.x = transformed_quat.x();
+    transformed_odom.pose.pose.orientation.y = transformed_quat.y();
+    transformed_odom.pose.pose.orientation.z = transformed_quat.z();
+    pub_state_estimation.publish(transformed_odom);
+    odom_cnt++;
+
+    if (odom_cnt % 10 == 0) {
+      odom_cnt = 0;
+      geometry_msgs::PoseStamped pose_stamped;
+      pose_stamped.header.stamp = msg->header.stamp;
+      pose_stamped.header.frame_id = world_frame_id;
+      pose_stamped.pose = transformed_odom.pose.pose;
+
+      path_msg.header.stamp = msg->header.stamp;
+      path_msg.header.frame_id = world_frame_id;
+      path_msg.poses.push_back(pose_stamped);
+      pub_path.publish(path_msg);
+    }
+
+    Eigen::Matrix4d T_base_world_base = T_world_base_base.inverse();
+    static tf::TransformBroadcaster br;
+    br.sendTransform(
+        tf::StampedTransform(convertEigenMatrixToTransform(T_base_world_base),
+                             msg->header.stamp, base_frame_id, world_frame_id));
+    return T_world_base_base;
+  }
+
+  // ROS
+  // clang-format off
+  ros::Subscriber sub_odometry;
+  // clang-format on
+
+  ros::Publisher pub_state_estimation;
+  ros::Publisher pub_path;
+  nav_msgs::Path path_msg;
+
+  std::unique_ptr<tf::TransformListener> tf_listener;
+
+  // Parameters
+  std::string loc_world_frame_id, loc_sensor_frame_id;
+  std::string world_frame_id, base_frame_id, sensor_frame_id;
+
+  // Others
+  bool init_system = false;
+  int odom_cnt = 0;
+  Eigen::Matrix4d T_base_sensor = Eigen::Matrix4d::Identity();
 };
